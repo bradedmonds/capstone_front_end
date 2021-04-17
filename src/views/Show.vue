be43d78e075219b2bdc382bc37294b2302d577f4
Brad Edmonds <bcedmonds0417@gmail.com>
2021-04-17 14:49:19 -0500

    make show pages display all concerts

--- src/views/Show.vue
+++ src/views/Show.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    
+    <p v-for="concert in concerts">
+      {{ concert.date }}
+      {{ concert.name }}
+      {{ concert.venue }}
+      {{ concert.tickets }}
+      </p>
   </div>
 </template>
 
@@ -13,15 +18,15 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Show Page",
-      cities: [],
+      message: `${this.$route.params.id} Concerts`,
+      concerts: [],
     };
   },
   created: function () {
     axios.get(`api/cities/${this.$route.params.id}`).then((response) => {
       console.log("doing the show");
       console.log(response.data);
-      this.cities = response.data;
+      this.concerts = response.data;
     });
   },
   methods: {},
